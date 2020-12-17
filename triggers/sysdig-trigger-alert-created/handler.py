3db334a78d22da5eedcb7a6ca3ac2998fb5dd22f
Kenaz Kwa <kenaz@puppetlabs.com>
2020-12-16 16:25:09 -0800

    Update handler.py

--- triggers/sysdig-trigger-alert-created/handler.py
+++ triggers/sysdig-trigger-alert-created/handler.py
@@ -4,14 +4,19 @@
 
 from relay_sdk import Interface, WebhookServer
 from quart import Quart, request, jsonify, make_response
+import logging
+import json
 
 relay = Interface()
 app = Quart('alert-created')
+logging.getLogger().setLevel(logging.INFO)
 
 @app.route('/', methods=['POST'])
 async def handler():
 
     payload = await request.get_json()
+    logging.info("Received the following webhook payload: \n%s", json.dumps(payload, indent=4))
+    
     if payload is None:
         return {'message': 'not a valid webhook'}, 400, {}
 
